4bf98070cfa004ecf8c059dd32f66966b2016eed
ValK-98 <vkornijenka@gmail.com>
2024-02-26 13:39:20 +1000

    configured createPaymentIntent

--- src/auth.js
+++ src/auth.js
@@ -8,3 +8,8 @@ dotenv.config()
 export function generateAccessToken(username) {
     return jwt.sign(username, process.env.TOKEN_SECRET, {}) // currently doesn't expire
   }
+
+
+// const username = { username: "x" }; 
+// const token = generateAccessToken(username);
+// console.log(token);
--- src/services/stripeService.js
+++ src/services/stripeService.js
@@ -1,10 +1,14 @@
-import stripe from '../config/stripeConfig.js';
+import stripe from "../config/stripeConfig.js";
 
 export const createPaymentIntent = async (paymentMethodId, amount) => {
   return stripe.paymentIntents.create({
     amount: amount,
-    currency: 'aud',
+    currency: "aud",
     payment_method: paymentMethodId,
     confirm: true,
+    automatic_payment_methods: {
+      enabled: true,
+      allow_redirects: "never",
+    },
   });
 };
