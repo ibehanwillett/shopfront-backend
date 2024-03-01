9a9175360dfd74ca22e5dfff1805f0c5cd6e5d2d
ibehanwillett <imogenbdubs@gmail.com>
2024-03-01 10:46:51 +1000

    commented index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import app from './app.js'
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+// Specifics the port that express should listen on. If no port is specified in the .env file port 4001 is used.
 app.listen(process.env.PORT || 4001)
