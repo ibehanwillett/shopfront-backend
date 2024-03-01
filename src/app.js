b4d88148b169d8958baf744ff6f210fc21832fe0
ibehanwillett <imogenbdubs@gmail.com>
2024-03-01 10:39:14 +1000

    commented app.js

--- src/app.js
+++ src/app.js
@@ -8,11 +8,14 @@ import paymentRoutes from './routes/paymentRoutes.js'
 
 dotenv.config();
 
-
+// Creates an express application
 const app = express()
-// we need this import so that other ports can access the database
+
+/* Defines the cross-orgin resource sharing. This permits browser requests orginating from 'https://nicolenightmare.xyz'
+to load reasources. Credentials are allowed, permitting access to cookies. */ 
+
 app.use(cors({
-    origin: 'https://nicolenightmare.xyz', //change to domain of deployed website post deployment
+    origin: 'https://nicolenightmare.xyz', 
     credentials: true,
     allowedHeaders: [
       "set-cookie",
@@ -22,16 +25,22 @@ app.use(cors({
     ]
 }))
 
+// Parses incoming requests with JSON payloads
 app.use(express.json())
 
+// Parses the cookie header & populates req.cookie with the on object keyed with "access_token" (the cookie's name) 
 app.use(cookieParser())
 
+// Intialize an endpoint, a GET request sent to '/' will return an object with the value 'Shopfront Backend' with the key of info
 app.get('/', (req, res) => res.send({ info: 'Shopfront Backend' }))
 
+// Implements all routes in the item_routes module with the prefix of '/items'
 app.use('/items', itemRoutes)
 
+// Implements all routes in the user_routes module with the prefix of '/users'
 app.use('/users', userRoutes)
 
+// Implements all routes in the paymentRoutes module with the prefix of '/payment'
 app.use('/payment', paymentRoutes);
 
 export default app
